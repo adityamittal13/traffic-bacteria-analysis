6d31e02e3c7a94ea6249ad922145b4548ddab01b
Aditya <adityamittal307@gmail.com>
2023-12-14 16:51:02 -0500

    add clustering plot

--- clustering.py
+++ clustering.py
@@ -1,6 +1,7 @@
 import feature_calc
 from sklearn.cluster import KMeans
 from scipy.spatial.distance import cdist
+from sklearn.decomposition import PCA
 import numpy as np
 import os
 import matplotlib.pyplot as plt
@@ -30,14 +31,12 @@ for fileidx, filename in enumerate(os.listdir('./ext-seg-imgs')):
     group_labels = np.append(group_labels, 0)
     images[fileidx + num_seg_images] = feature_calc.feature_calculation(f"./ext-seg-imgs/{filename}")
 
-
 distortions = []
 inertias = []
 K = range(1, 100)
 
 for k in K:
     kmeanModel = KMeans(n_clusters = k, init="k-means++", random_state=random_seed, n_init="auto").fit(images)
-
     distortions.append(sum(np.min(cdist(images, kmeanModel.cluster_centers_,
                                         'euclidean'), axis=1)) / images.shape[0])
     inertias.append(kmeanModel.inertia_)
@@ -67,3 +66,16 @@ clusters = {idx: set() for idx in range(len(groups))}
 for idx in range(images.shape[0]):
     clusters[group_labels[idx]].add(targets[idx])
 print(list(map(lambda x: len(clusters[x]), clusters.keys())))
+
+kmeans_plot = KMeans(n_clusters=10, init="k-means++", random_state=42, n_init="auto").fit(images)
+targets_plot = kmeans_plot.labels_
+reduced_images = PCA(2).fit_transform(images)
+u_labels = np.unique(targets_plot)
+for i in u_labels:
+    plt.scatter(reduced_images[targets_plot == i, 0], reduced_images[targets_plot == i, 1], label = i)
+plt.legend()
+plt.xlabel('PCA Dim 1')
+plt.ylabel('PCA Dim 2')
+plt.title('PCA Plot of Clusters (k = 10)')
+plt.savefig("kmeans_plot.jpg")
+plt.clf()
